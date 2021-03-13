f8a9f764d68a0d3370342cc93f675ecd58a743d0
TheKennyman07 <69918528+KennyMan14@users.noreply.github.com>
2021-03-13 14:26:07 -0600

    Update role.upgrader.js

--- role.upgrader.js
+++ role.upgrader.js
@@ -1,18 +1,17 @@
 var roleUpgrader = {
 
-    /** @param {Creep} creep **/
     run: function(creep) {
 
         if(creep.memory.upgrading && creep.store[RESOURCE_ENERGY] == 0) {
             creep.memory.upgrading = false;
             creep.say('🔄 harvest');
 	    }
-	    if(!creep.memory.upgrading && creep.store.getFreeCapacity() == 0) {
+        }if(!creep.memory.upgrading && creep.store.getFreeCapacity() == 0) {
 	        creep.memory.upgrading = true;
 	        creep.say('⚡ upgrade');
-	    }
+	}
 
-	    if(creep.memory.upgrading) {
+	if(creep.memory.upgrading) {
             if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(creep.room.controller, {visualizePathStyle: {stroke: '#00ffbd'}});
             }
@@ -23,7 +22,7 @@ var roleUpgrader = {
                 creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#c1f7ff'}});
             }
         }
-	}
+    }
 };
 
 module.exports = roleUpgrader;
