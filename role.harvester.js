a0fad91ca9cc66f072488365bb3ffa67043e2744
TheKennyman07 <69918528+KennyMan14@users.noreply.github.com>
2021-03-13 14:19:49 -0600

    Create role.harvester.js

---
+++ role.harvester.js
@@ -0,0 +1,40 @@
+var roleUpgrader = require('role.upgrader');
+
+var roleHarvester = {
+  
+    run: function(creep) {
+        
+	    if (creep.memory.harvesting && creep.store[RESOURCE_ENERGY] == 0) {
+            creep.memory.harvesting = false;
+        }
+        else if (!creep.memory.harvesting && creep.store.getFreeCapacity() == 0) {
+            creep.memory.harvesting = true;
+        }
+
+        if (creep.memory.harvesting) {
+            var str = creep.pos.findClosestByPath(FIND_MY_STRUCTURES, {
+                filter: (s) => (s.structureType == STRUCTURE_SPAWN
+                             || s.structureType == STRUCTURE_EXTENSION
+                             || s.structureType == STRUCTURE_TOWER)
+                             && s.store.getFreeCapacity(RESOURCE_ENERGY) > 0
+            });
+
+            if (str) {
+                if (creep.transfer(str, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(str, {visualizePathStyle: {stroke: '#84ff75'}});
+                }
+            } else{
+                roleUpgrader.run(creep);
+            }
+        }
+        else {
+            var sr = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE);
+            if (creep.harvest(sr) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(sr, {visualizePathStyle: {stroke: '#eaff75'}});
+            }
+        }
+    }
+};
+
+module.exports = roleHarvester;
+
