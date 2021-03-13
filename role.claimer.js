16a5f48a90e1ca0d6b914d418b38d49cb4f506ea
TheKennyman07 <69918528+KennyMan14@users.noreply.github.com>
2021-03-13 16:17:11 -0600

    Create role.claimer.js

---
+++ role.claimer.js
@@ -0,0 +1,14 @@
+var roleClaimer = {
+    run: function(creep) {
+        if (creep.room.name !== creep.memory.target) {
+            var exit = creep.room.findExitTo(creep.memory.target);
+            creep.moveTo(creep.pos.findClosestByRange(exit));
+        }
+        else {
+            if (creep.claimController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(creep.room.controller);
+            }
+        }
+    }
+};
+module.exports = roleClaimer;
